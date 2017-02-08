0caa810713e55cecd175730a323c096434b56346
Ryuji Ganaha <RyujiGanaha@MacBook-Pro.local>
2017-02-09 07:43:04 +0900

    implementing authenticator in service

---
+++ app/services/staff/authenticator.rb
@@ -0,0 +1,20 @@
+class Staff::Authenticator
+  def initialize(staff_member)
+    @staff_member = staff_member
+  end
+
+  def authenticate(raw_password)
+    # staff_memberがnilじゃなくて
+    # 有効で
+    # パスワードが設定されていて
+    # 開始日が今日以前、
+    # なお終了日が設定されていあくて
+    # 渡されたパスワードが正しければtrueを返す
+    @staff_member &&
+      !@staff_member.suspended? &&
+      @staff_member.hashed_password &&
+      @staff_member.start_date <= Date.today &&
+      (@staff_member.end_date.nil? || @staff_member.end_date > Date.today) &&
+      BCrypt::Password.new(@staff_member.hashed_password) == raw_password
+  end
+end
---
+++ spec/services/staff/authenticator_spec.rb
@@ -0,0 +1,38 @@
+require 'spec_helper'
+
+describe Staff::Authenticator do
+  describe '#authenticate' do
+
+    example 'Should return true if correct password' do
+      m = build(:staff_member)
+      expect(Staff::Authenticator.new(m).authenticate('pw')).to be_truthy
+    end
+
+    example 'Should return false if incorrect password' do
+      m = build(:staff_member)
+      expect(Staff::Authenticator.new(m).authenticate('ashdahsud')).to be_falsey
+    end
+
+    example 'Should return false if password not set' do
+      m = build(:staff_member, password: nil)
+      expect(Staff::Authenticator.new(m).authenticate(nil)).to be_falsey
+    end
+
+    example 'Should return false suspended' do
+      m = build(:staff_member, suspended: true)
+      expect(Staff::Authenticator.new(m).authenticate('pw')).to be_falsey
+    end
+
+    example 'Should return false if not yet started' do
+      m = build(:staff_member, start_date: Date.tomorrow)
+      expect(Staff::Authenticator.new(m).authenticate('pw')).to be_falsey
+    end
+
+    example 'Should return false if passed end date' do
+      m = build(:staff_member, end_date: Date.yesterday)
+
+      expect(Staff::Authenticator.new(m).authenticate('pw')).to be_falsey
+    end
+
+  end
+end
