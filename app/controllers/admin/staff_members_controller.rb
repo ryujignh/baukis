78396f9f84dca9d0573159659bbb87ec7eb1aec9
Ryuji Ganaha <RyujiGanaha@MacBook-Pro.local>
2017-02-11 10:33:09 +0900

    adding new action

--- app/controllers/admin/staff_members_controller.rb
+++ app/controllers/admin/staff_members_controller.rb
@@ -1,6 +1,17 @@
 class Admin::StaffMembersController < Admin::Base
+
   def index
     @staff_members = StaffMember.order(:family_name_kana, :given_name_kana)
+  end
+
+  # updateで失敗した場合、URLを他のブラウザに貼り付けてアクセスするとshowへアクセスが走るので、リダイレクトする
+  def show
+    staff_member = StaffMember.find(params[:id])
+    redirect_to [ :edit, :admin, staff_member ]
+  end
+
+  def new
+    @staff_member = StaffMember.new
   end
 
 end
