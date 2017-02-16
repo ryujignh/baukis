fb20e3b922c313234ced78e982cae3674c718c26
Ryuji Ganaha <RyujiGanaha@MacBook-Pro.local>
2017-02-16 21:18:05 +0900

    new feature. staff can update its information

--- db/seeds/development/staff_members.rb
+++ db/seeds/development/staff_members.rb
@@ -1,10 +1,10 @@
 StaffMember.create!(
-	email: 'taro@example.com',
+	email: 'staff@example.com',
 	family_name: '山田',
 	given_name: '太郎',
 	family_name_kana: 'ヤマダ',
 	given_name_kana: 'タロウ',
-	password: 'password',
+	password: 'staff',
 	start_date: Date.today,
 	)
 
