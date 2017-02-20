76a2e3d2731447bc1002bff63d111dbdec13ac16
Ryuji Ganaha <RyujiGanaha@MacBook-Pro.local>
2017-02-21 08:29:05 +0900

    cleaning up

--- app/controllers/admin/staff_events_controller.rb
+++ app/controllers/admin/staff_events_controller.rb
@@ -4,14 +4,16 @@ class Admin::StaffEventsController < Admin::Base
     # For specific staff member's event
     if params[:staff_member_id]
       @staff_member = StaffMember.find(params[:staff_member_id])
-      @events = @staff_member.events.order(occurred_at: :desc)
+      @events = @staff_member.events
     else
       # For all staff members events
-      @events = StaffEvent.order(occurred_at: :desc)
+      @events = StaffEvent
     end
-    # for pagination
+    @events = @events.order(occurred_at: :desc)
+                .includes(:member).page(params[:page])
+    # .includes for N + 1 problem
+    # .page for pagination
     # @eventsオブジェクトににLIMIT節を加えて、新しくクエリを発行している
     # StaffEvent Load (0.2ms)  SELECT  `staff_events`.* FROM `staff_events`  WHERE `staff_events`.`staff_member_id` = 1 LIMIT 10 OFFSET 0
-    @events = @events.page(params[:page])
   end
 end
