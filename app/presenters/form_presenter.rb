f4c76150f35183fc7720a10e8d2a683ee8aa9ae8
Ryuji Ganaha <RyujiGanaha@MacBook-Pro.local>
2017-02-24 07:53:53 +0900

    cleaning up form_presenter

--- app/presenters/form_presenter.rb
+++ app/presenters/form_presenter.rb
@@ -22,35 +22,36 @@ class FormPresenter
 
   def text_field_block(name, label_text, options = {})
     markup(:div, class: 'input-block') do |m|
-      m << label(name, label_text,
-        class: options[:required] ? 'required' : nil)
+      m << decorated_label(name, label_text, options)
       m << text_field(name, options)
     end
   end
 
   def password_field_block(name, label_text, options = {})
     markup(:div, class: 'input-block') do |m|
-      m << label(name, label_text,
-        class: options[:required] ? 'required' : nil)
+      m << decorated_label(name, label_text, options)
       m << password_field(name, options)
     end
   end
 
   def date_field_block(name, label_text, option = {})
     markup(:div, class: 'input-block') do |m|
-      m << label(name, label_text,
-        class: options[:required] ? 'required' : nil)
+      m << decorated_label(name, label_text, options)
       m << text_field(name, options)
     end
   end
 
   def full_name_block(name1, name2, label_text, options = {})
     markup(:div, class: 'input-block') do |m|
-      m << label(name1, label_text,
-        class: options[:required ? 'required' : nil])
+      m << decorated_label(name1, label_text, options)
       m << text_field(name1, options)
       m << text_field(name2, options)
     end
+  end
+
+  private
+  def decorated_label(name, label_text, options = {})
+    label(name, label_text, class: options[:required ? 'required' : nil])
   end
 
 end
