c0427cf29e0413dfc0c2b923e0cc111ce56ef48c
Ryuji Ganaha <RyujiGanaha@MacBook-Pro.local>
2017-02-24 08:35:08 +0900

    implementing error message hander in form presenter

--- app/presenters/form_presenter.rb
+++ app/presenters/form_presenter.rb
@@ -24,6 +24,7 @@ class FormPresenter
     markup(:div, class: 'input-block') do |m|
       m << decorated_label(name, label_text, options)
       m << text_field(name, options)
+      m << error_messages_for(name)
     end
   end
 
@@ -31,6 +32,7 @@ class FormPresenter
     markup(:div, class: 'input-block') do |m|
       m << decorated_label(name, label_text, options)
       m << password_field(name, options)
+      m << error_messages_for(name)
     end
   end
 
@@ -38,14 +40,21 @@ class FormPresenter
     markup(:div, class: 'input-block') do |m|
       m << decorated_label(name, label_text, options)
       m << text_field(name, options)
+      m << error_messages_for(name)
     end
   end
 
-  def full_name_block(name1, name2, label_text, options = {})
-    markup(:div, class: 'input-block') do |m|
-      m << decorated_label(name1, label_text, options)
-      m << text_field(name1, options)
-      m << text_field(name2, options)
+  ###########################################################
+  # Error message handler
+  ###########################################################
+
+  def error_messages_for(name)
+    markup do |m|
+      object.errors.full_messages_for(name).each do |message|
+        m.div(class: 'error-message') do |m|
+          m.text message
+        end
+      end
     end
   end
 
